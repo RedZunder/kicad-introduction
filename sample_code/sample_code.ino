5c3ab3ec9e7eb220343ae0cd787c9adec4bc4865
RedZunder <alt47r@gmail.com>
2026-06-02 12:21:17 +0200

    Updated sample code and design

--- sample_code/sample_code.ino
+++ sample_code/sample_code.ino
@@ -1,30 +1,30 @@
 
-#define PB4 4
-
-
 void setup() {
   // put your setup code here, to run once:
   pinMode(PB4,OUTPUT);
-  if(!1)//mpu.begin())
+  pinMode(PB3,INPUT);
+
+  uint8_t c=0;
+
+  //For info, blink the LED 5 times
+  while(c<5)  
   {
-    while(1)
-    {
-      digitalWrite(PB4, HIGH);
-      delay(300);
-      digitalWrite(PB4, LOW);
-      delay(300);
-    }
+    digitalWrite(PB4, HIGH);
+    delay(500);
+    digitalWrite(PB4, LOW);
+    delay(500);
+    c++;
   }
 
 }
 
 
-int potentiometerValue=0;
-
+int valueRead=0;
+int pwmValue=0;
 void loop() {
   // put your main code here, to run repeatedly:
-  analogRead(PB1)
-
-
-
+  valueRead=analogRead(PB3);
+  pwmValue=valueRead*255/600;
+  analogWrite(PB4, pwmValue);
+  delay(100);
 }
